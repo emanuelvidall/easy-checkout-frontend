0d8c44e7bb22871dcddd69817b77407a8ae24e79
Emanuel Vidal <emanuelvidalrm@gmail.com>
2024-12-03 16:23:33 -0300

    :rotating_light: removed lint warnings

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import { SidebarProvider } from "@/components/ui/sidebar";
 import { Toaster } from "@/components/ui/sonner";
 import SidebarWrapper from "@/components/sidebar-wrapper";
 
